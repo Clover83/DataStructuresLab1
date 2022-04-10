72b30737d939beebf5592438599384d84a1de095
Lukas Aukstakalnis <93783954+Lu4asz@users.noreply.github.com>
2022-04-10 17:49:15 +0200

    Random generator for nodes that will be used as a parameter for sorting algorithm

--- DataStructuresLab1/DataStructuresLab1.cpp
+++ DataStructuresLab1/DataStructuresLab1.cpp
@@ -2,10 +2,57 @@
 //
 
 #include <iostream>
+using namespace std;
+
+class Node {
+public:
+	int Value;
+	Node* Next;
+};
+
+void printList(Node* n) {
+	while (n != nullptr) {
+		cout << n->Value << endl;
+		n = n->Next;
+	}
+}
+
+void insertAtTheEnd(Node** head, int newValue) {
+	//create a new node
+	Node* newNode = new Node();
+	newNode->Value = newValue;
+	newNode->Next = nullptr;
+	if (*head == nullptr) {
+		*head = newNode;
+		return;
+	}
+	//look for the last node
+	Node* Last = *head;
+	while (Last->Next != nullptr) {
+		Last = Last->Next;
+	}
+	//insert newnode at the end
+	Last->Next = newNode;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //cout << "Hello World!\n";
+
+	//Run independant random
+	srand(time(0));
+	Node* head = new Node();
+	head->Value = rand();
+	int numberOfNodes = 10000;
+
+	for (size_t i = 0; i < numberOfNodes; i++)
+	{
+		int bob = rand();
+		insertAtTheEnd(&head, bob);
+	}
+
+	printList(head);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
@@ -15,6 +62,6 @@ int main()
 //   1. Use the Solution Explorer window to add/manage files
 //   2. Use the Team Explorer window to connect to source control
 //   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
+//   4. Use the Error Listfsd window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
