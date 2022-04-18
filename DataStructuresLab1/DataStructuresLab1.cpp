c455f7219dc120b518194ffec2fab77ef159677d
Lukas Aukstakalnis <93783954+Lu4asz@users.noreply.github.com>
2022-04-18 11:31:35 +0200

    lol

--- DataStructuresLab1/DataStructuresLab1.cpp
+++ DataStructuresLab1/DataStructuresLab1.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <math.h>
 
 using namespace std;
-// Singly linked node class
+
 class SNode {
 public:
 	int Value;
@@ -18,6 +17,46 @@ void printList(SNode* n) {
 		n = n->Next;
 	}
 }
+
+SNode* mid_node(SNode* start, SNode* last, int& middlepos) {
+	if (start == NULL)
+		return NULL;
+	SNode* slow = start;//head
+	SNode* fast = start->Next;//next88
+	middlepos++;
+	while (fast != last->Next and last->Next != nullptr) { //while not null
+		fast = fast->Next; //take next 98
+		if (fast != last->Next && last->Next != nullptr) { //if not null
+			slow = slow->Next; //
+			middlepos++;
+			fast = fast->Next;
+		}
+	}
+	if (last->Next == nullptr) {
+		while (fast != last) {
+			fast = fast->Next;
+			if (fast != last) {
+				slow = slow->Next;
+				fast = fast->Next;
+				middlepos++;
+			}
+		}
+	}
+	cout << endl;
+	cout << endl;
+	cout << "middle position " << middlepos;
+	cout << endl;
+	cout << endl;
+	cout << "middle value" << slow->Value << endl;
+	cout << endl;
+	cout << endl;
+	cout << "unsorted value" << last->Value;
+	return slow;
+}
+
+
+
+
 void SinglyGenerator(SNode** head, int newValue) {
 	SNode* newNode = new SNode();
 	newNode->Value = newValue;
@@ -25,116 +64,121 @@ void SinglyGenerator(SNode** head, int newValue) {
 	*head = newNode;
 }
 
-// Doubly linked node class
-class DNode {
-public:
-	int Value;
-	DNode* Next;
-	DNode* Prev;
-};
+int BinarySearch(SNode* head, SNode* temp, int value, int sortedlen) { //int value is the unsorted value
+	SNode* last = temp;
+	SNode* start = head;
+	//int upperbound = sortedlen;
+	//int lowerbound = 1;
+	int bob = 0;
+	do {
+		int middlepos = 0;
+		SNode* mid = mid_node(start, last, middlepos);
+		int midvalue = middlepos;
+		if (mid == NULL)
+			return NULL;
+		if (mid->Value == value) { //unsorted == mid
+			break;
+		}
+		else if (mid->Value < value) {
+			if (mid->Next->Value != value) {
+				start = mid->Next;
+				//lowerbound = middlepos;
+			}// unsorted > mid
+			else {
+				bob = 1;
+				mid->Value;
+				break;
 
-
-
-
-void bSort(SNode* singlyHead) {
-	int sortedLength = 0;
-	int upperBound = 0;
-	int toWalk = 0; // prob dont need
-	SNode* newHead = singlyHead;
-	while (singlyHead->Next != nullptr) {
-		if (sortedLength == 0) {
-			sortedLength++;
+			}
 		}
 		else {
-		
-			SNode* middleNode = newHead;
-			SNode* unsorted = singlyHead->Next;
-			int newUnsortedValue = unsorted->Value;
-			//
-			//veikia..? binary atlieka
-			upperBound = sortedLength;
-			int lowerBound = 0;
-			while ((upperBound - lowerBound) != 0) {
-				int middle = (floor(upperBound-lowerBound)/ 2);
+			last = mid;
+			if (middlepos == 1) {
+				bob = 2;
+				if (temp->Next != nullptr) {
 
-				for (size_t i = 0; i < middle; i++)
-				{
-					if (middle == 0)
-						middle = middleNode->Value;
-					else
-						if(middleNode->Next != nullptr)
-							middleNode = middleNode->Next;
-						break;
+					for (size_t i = 1; i < sortedlen; i++)
+					{
+						head = head->Next; //get tail
+					}
+					head->Value;
 				}
-				if (middleNode->Value == newUnsortedValue) {
-					break;
-				}
-				if (middleNode->Value > newUnsortedValue) {
-					//to the left
-					cout << "to left" << endl;
-					upperBound = middle;
-				}
-				if (middleNode->Value < newUnsortedValue) {
-					//to the right
-					cout << "to right" << endl; //ceil
-					lowerBound = (floor(upperBound - lowerBound) / 2)+1; //+1 maybe maybe not, ceil not doing as intended
-					
-				}
+
+				//SNode* tailnext = temp->Next;
+				//tailnext->Value = temp->Next->Value;
+				//head->Next = tailnext; //tail go to temp next
+				//SNode* midnext = mid->Next;
+
+				//temp = mid->Next; //temp is midnext
+				//temp->Next = mid;
+				//mid->Next = midnext;
+				//cout << endl;
+				//printList(head);
+				//SNode* temp = currY->next;
+				//currY->next = currX->next;
+				//currX->next = temp;
+				break;
 			}
-		if (middleNode->Value > newUnsortedValue) {
-			//SNode* temp = singlyHead->Next;
+		} //unsorted < mid
+	} while (true);
 
+	if (bob == 2) {
+		cout << "left";
 
-
-		}
-		if (middleNode->Value < newUnsortedValue)
-		{
-			//nzn, kazka blogai darau cia
-			
-			cout << "entered";
-			//sitas turbut ne
-			/*SNode* temp = unsorted;
-			unsorted->Next = middleNode->Next;
-			middleNode->Next = unsorted;
-			for (size_t i = 1; i < sortedLength; i++)
-			{
-				newHead->Next;
-				cout << sortedLength;
-			}
-			newHead->Next = temp->Next;*/
-			//SNode* unsorted = singlyHead;
-
-			//nop
-			/*singlyHead = singlyHead->Next->Next;
-			unsorted->Next = middleNode->Next;
-			middleNode->Next = unsorted->Next;*/
-
-
-			//? irgi ne
-			//SNode* temp = unsorted;
-			//unsorted->Next = middleNode->Next;
-			//middleNode->Next = unsorted;
-			//newHead->Next->Next = temp->Next;
-			
-		}
-		sortedLength++;
-		singlyHead = singlyHead->Next;
-		}
 	}
+	if (bob == 1) {
+
+		//for (size_t i = 1; i < sortedlen; i++)
+		//{
+		//	head = head->Next;
+		//}
+		//head->Next = temp->Next;
+		//temp->Next=
+		cout << "right";
+
+	}
+	if (bob == 0) {
+		cout << "same";
+
+	}
+
 }
+
+
+void bSort(SNode* head) {
+
+
+	SNode* temp = head;
+	SNode* start = head;
+	int sortedLenght = 0;
+	if (sortedLenght == 0 && start->Next != nullptr) {
+		sortedLenght++;
+		temp = temp->Next;
+	}
+	while (temp != nullptr) {
+		BinarySearch(head, temp, temp->Value, sortedLenght);
+
+		//end
+		temp = temp->Next;
+		sortedLenght++;
+	}
+
+
+
+}
+
+
 
 
 int main() {
 
-	//Run indepentant random values
+
 	srand(time(0));
 
 	//Makes first node for singly linked
 	SNode* singlyHead = new SNode();
-	singlyHead->Value = rand();
-	//Makes first node for doubly
-	//DNode* doublyHead = new DNode();
-	//doublyHead->Value = 15;
+	singlyHead->Value = 10;
+
 
 	//Number of Nodes
 	int numberOfNodes = 10;
@@ -142,15 +186,21 @@ int main() {
 	for (size_t i = 0; i < numberOfNodes; i++)
 	{
 		int randomValue = rand();
-		//Uncomment when need to generate
+
 
 		SinglyGenerator(&singlyHead, randomValue);
 
-		//DoublyGenerator(&doublyHead,randomValue);
-
 	}
 	printList(singlyHead);
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
 	bSort(singlyHead);
-	cout << endl << "print after";
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
 	printList(singlyHead);
+	return 0;
 }
